5710d737ecebcd0a3242e119f27c7b3645ad24e4
Kaushik1210 <kabikaushik1210@gmail.com>
2024-01-17 18:48:39 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signout from './pages/Signout'
+import Signin from './pages/Signin'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl text-red-300'>MERN stack</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>}/>
+          <Route path='/sign-in' element={<Signin/>}/>
+          <Route path='/sign-out' element={<Signout/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
